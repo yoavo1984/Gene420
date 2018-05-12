ac180812e0c4170b2626a670f2d7e2a63eb279bd
Mark Fainstein <mark@slatescience.com>
2018-05-12 12:46:46 +0300

    minor

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -17,9 +17,18 @@ export class UserComponent implements OnInit {
   private photoUrl="";
   private currentStrainView;
   private loggedIn:boolean = false;
-  public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-  public doughnutChartData:number[] = [350, 450, 100];
-  public doughnutChartType:string = 'doughnut';
+  public barChartOptions:any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType:string = 'bar';
+  public barChartLegend:boolean = true;
+
+  public barChartData:any[] = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
 
   constructor(private authService:AuthService, private usersDao:UserDaoService, private router:Router) {
   }
@@ -47,7 +56,7 @@ export class UserComponent implements OnInit {
       this.photoUrl = user.photoUrl;
       this.email = user.email;
       this.uid = user.uid;
-      this.fetchGenetics(uid);
+      this.fetchGenetics(this.uid);
     })
   }
 
@@ -71,6 +80,7 @@ export class UserComponent implements OnInit {
       }
       this.userGeneticsReady = true;
     });*/
+    });
   }
 
 }
